e7805fe93138eb52fd1b2efa9db47be5163ca5b5
bernborgess <bernborgesse@outlook.com>
2025-08-30 11:25:51 -0300

    Initialize the fruehling spring project

---
+++ fruehling/src/main/java/com/example/fruehling/FruehlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.fruehling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruehlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruehlingApplication.class, args);
+	}
+
+}
